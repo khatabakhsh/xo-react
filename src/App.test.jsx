9cbd774508a80c4db14fb5657697315c012f0371
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-04-06 19:03:31 +0430

    write prettier to files

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+React.test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  React.expect(linkElement).toBeInTheDocument();
 });
