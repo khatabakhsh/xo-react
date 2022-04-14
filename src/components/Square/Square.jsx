63bf298e86f4fe84923555e1220733d076783fed
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-04-15 02:02:39 +0430

    square rendered with onClick

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,9 +1,19 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import styles from './styles.module.scss';
 
-function Square({ letter }) {
-  return <span className={`${styles.span}`}>{letter}</span>;
+function Square({ letter, setLetters, index }) {
+  return (
+    <span
+      role="button"
+      tabIndex={index}
+      onClick={() => setLetters((prev) => ({ ...prev, [index]: 'x' }))}
+      className={`${styles.span}`}
+    >
+      {letter}
+    </span>
+  );
 }
 
 export default Square;
--- src/containers/Game/Game.jsx
+++ src/containers/Game/Game.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Square, Grid } from '../../components';
 
 function Game() {
+  const [letters, setLetters] = useState({
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+    5: '',
+    6: '',
+    7: '',
+    8: '',
+    9: '',
+  });
   return (
     <Grid>
-      <Square letter="x" />
-      <Square letter="x" />
-      <Square letter="x" />
-      <Square letter="x" />
-      <Square letter="x" />
-      <Square letter="x" />
-      <Square letter="x" />
-      <Square letter="x" />
-      <Square letter="x" />
+      {Object.keys(letters).map((letterIndex) => {
+        return (
+          <Square
+            letter={letters[letterIndex]}
+            setLetters={setLetters}
+            index={Number(letterIndex)}
+            key={Number(letterIndex)}
+          />
+        );
+      })}
     </Grid>
   );
 }
