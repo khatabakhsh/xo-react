bfd63470c23b7f8204f94597f83532d4b9c4104d
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-04-23 17:04:11 +0430

    Start add to Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import { PageLayout } from './components';
-import { Header, Main, Footer } from './containers';
+import { Header, Main, Footer, Start } from './containers';
 
 function App() {
   const [scores, setScores] = useState({ firstPlayer: 0, secondPlayer: 0 });
@@ -21,6 +21,7 @@ function App() {
           path="game"
           element={<Main scores={scores} resetScores={resetScores} />}
         />
+        <Route path="/" element={<Start />} />
       </Routes>
       <Footer />
     </PageLayout>
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,6 @@
 import Footer from './footer/Footer';
 import Header from './header/Header';
 import Main from './main/Main';
+import Start from './start/start';
 
-export { Header, Footer, Main };
+export { Header, Footer, Main, Start };
