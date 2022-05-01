b8a3dbb18d9bb20947be23d483e90d440143dd9d
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-05-01 13:23:23 +0430

    start.jsx => Start.jsx

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,6 @@
 import Footer from './footer/Footer';
 import Header from './header/Header';
 import Main from './main/Main';
-import Start from './start/start';
+import Start from './start/Start';
 
 export { Header, Footer, Main, Start };
