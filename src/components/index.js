ea3d332bbf81cee7ffebfebeb928d087b7f3a843
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-05-01 13:27:35 +0430

    add .jsx to imports (for vervel host)

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
-import Button from './button/Button';
-import Square from './square/Square';
-import PageLayout from './pageLayout/PageLayout';
-import Grid from './grid/Grid';
-import Info from './info/Info';
-import Input from './input/Input';
+/* eslint-disable import/extensions */
+import Button from './button/Button.jsx';
+import Square from './square/Square.jsx';
+import PageLayout from './pageLayout/PageLayout.jsx';
+import Grid from './grid/Grid.jsx';
+import Info from './info/Info.jsx';
+import Input from './input/Input.jsx';
 
 export { Square, Button, PageLayout, Grid, Info, Input };
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,7 @@
-import Footer from './footer/Footer';
-import Header from './header/Header';
-import Main from './main/Main';
-import Start from './start/Start';
+/* eslint-disable import/extensions */
+import Footer from './footer/Footer.jsx';
+import Header from './header/Header.jsx';
+import Main from './main/Main.jsx';
+import Start from './start/Start.jsx';
 
 export { Header, Footer, Main, Start };
