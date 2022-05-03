527cfa9e997d3fb0d73178f4535e37398621534e
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-05-03 11:56:27 +0430

    preload font before display for Square Component zero-delay

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.scss';
 import { PageLayout } from './components';
 import { Header, Main, Footer, Start } from './containers';
+import woff from './assets/fonts/Merienda/merienda-v14-latin-ext_latin-700.woff';
+import woff2 from './assets/fonts/Merienda/merienda-v14-latin-ext_latin-700.woff2';
 
 function App() {
   // remove browser addressbar on mobiles
@@ -35,6 +37,22 @@ function App() {
         />
       </Routes>
       <Footer />
+
+      {/* preload font before display for Square Component zero-delay */}
+      <link
+        rel="preload"
+        as="font"
+        href={woff}
+        type="font/woff"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        as="font"
+        href={woff2}
+        type="font/woff2"
+        crossOrigin="anonymous"
+      />
     </PageLayout>
   );
 }
