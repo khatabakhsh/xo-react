d509f46b2182776b2e5617b9072409c67e3d0e69
Amirhossein_Khatabakhsh <khatabakhsha@gmail.com>
2022-05-12 01:14:01 +0430

    redux-devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { legacy_createStore as createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ devToolsEnhancer()
+);
 
 export default store;
